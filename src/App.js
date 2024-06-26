0a83fdc6e3e366cbba04c092a963e8dca6e2c490
dhruvi-tntra <dhruvi.patel@tntra.io>
2024-06-26 22:59:02 +0530

    Changes for task item create and delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import TodoInput from './components/TodoInput';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+   const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) =>{
+      return todo !==text;
+    });
+    setTodos(newTodos);
+   }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React To-Do</h1>
+      <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+      <TodoList list={todos} remove={deleteTodo}></TodoList>
     </div>
   );
 }
