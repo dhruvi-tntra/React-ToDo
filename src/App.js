de8021e72b12c8a24be69ca62d602b29896acb94
dhruvi-tntra <dhruvi.patel@tntra.io>
2024-06-27 11:02:05 +0530

    Added changes related to task completion

--- src/App.js
+++ src/App.js
@@ -9,23 +9,29 @@ function App() {
   
   const addTodo = () => {
     if (todo !== "") {
-      setTodos([...todos, todo]);
+      setTodos([...todos, { text: todo, completed: false }]);
       setTodo("");
     }
   };
 
    const deleteTodo = (text) => {
-    const newTodos = todos.filter((todo) =>{
-      return todo !==text;
-    });
+    const newTodos = todos.filter((todo) => todo.text !== text);
     setTodos(newTodos);
-   }
+  };
+
+
+   const completedTodo = (text) => {
+    const newTodos = todos.map((todo) => 
+      todo.text === text ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
       <h1>React To-Do</h1>
       <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
-      <TodoList list={todos} remove={deleteTodo}></TodoList>
+      <TodoList list={todos} remove={deleteTodo} complete={completedTodo} ></TodoList>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,44 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-function TodoList({list, remove}) {
-	return (
-		<>
-			{list?.length > 0 ? (
-				<ul className='todo-list'>
-					{list.map((entry, index) => (
-						<div className='todo'>
-							<li key={index}> {entry} </li>
-
-							<button 
-								className='delete-button'
-								onClick={() => {
-									remove(entry);
-								}}
-							>
-								Delete
-							</button>
-
-							<button 
-								className='completed-button'
-								onClick={() => {
-									remove(entry);
-								}}
-							>
-								Completed
-							</button>
-						</div>
-					)
-				)}
-				</ul>
-			) : (
-				<div className='empty'>
-					<p>
-						No task found!!!
-					</p>
-				</div>
-			)}
-		</>
-	);
+function TodoList({ list, remove, complete }) {
+  return (
+    <>
+      {list?.length > 0 ? (
+        <ul className='todo-list'>
+          {list.map((entry, index) => (
+            <div className='todo' key={index}>
+              <li>
+                <span style={{ textDecoration: entry.completed ? 'line-through' : 'none' }}>
+                  {entry.text}
+                </span>
+              </li>
+              <button 
+                className='delete-button'
+                onClick={() => remove(entry.text)}
+              >
+                Delete
+              </button>
+              <button 
+                className='completed-button'
+                onClick={() => complete(entry.text)}
+              >
+                {entry.completed ? 'Undo' : 'Completed'}
+              </button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className='empty'>
+          <p>No task found!!!</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default TodoList;
